00c30cec5a639b857feabb06cedcc302b876980f
Kimhak Oul <137867485+KimHak131@users.noreply.github.com>
2025-05-29 21:35:41 +0700

    update 1

--- index.js
+++ index.js
@@ -1,33 +1,20 @@
 const e1 = require('express');
 const app = e1();
 
-//REG API
-app.post('/registration', (req, res) => {
-    res.send('<html><body>INSIDE REGISTRATION API..</body></html>');
+// use proxy server to redirect the incoming request
+const httpProxy = require('http-proxy');
+const proxy = httpProxy.createProxyServer({});
+
+// redirect to the student micro-service
+app.use('/student', (req, res) => {
+    console.log("INSIDE API GATEWAY STUDENT ROUTE");
+    proxy.web(req, res, { target: 'http://localhost:5005' });
 });
 
-app.post('/login', (req, res) => {
-    res.send('<html><body>INSIDE LOGIN API..</body></html>');
-});
-
-//SEARCH API
-app.get('/search', (req, res) => {
-    res.send('<html><body>INSIDE SEARCH API..</body></html>');
-});
-
-//VIEW API
-app.get('/view', (req, res) => {
-    res.send('<html><body>INSIDE VIEW API..</body></html>');
-});
-
-//UPDATE API
-app.put('/updateprofile', (req, res) => {
-    res.send('<html><body>INSIDE UPDATE PROFILE API..</body></html>');
-});
-
-//DELETE API
-app.delete('/del', (req, res) => {
-    res.send('<html><body>INSIDE DELETE API..</body></html>');
+// redirect to the teacher micro-service
+app.use('/teacher', (req, res) => {
+    console.log("INSIDE API GATEWAY TEACHER ROUTE");
+    proxy.web(req, res, { target: 'http://localhost:5010' });
 });
 
 // START THE EXPRESS SERVER. 5000 is the PORT NUMBER
